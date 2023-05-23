2537e80138545d803bbaab13dbdd785228d03b3a
Ghassen666 <ghassenchaieb97@gmail.com>
2023-05-23 18:35:32 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -172,3 +172,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+# Existing code...
+
+# Add two newlines at the end of the file
